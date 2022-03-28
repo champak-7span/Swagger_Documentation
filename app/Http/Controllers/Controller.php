9a8a0e8e8aa0f7e90f2f947b568e93bc23c52333
champak <champak@7span.com>
2022-03-28 14:17:08 +0530

    Working on swagger doc

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,51 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+
 class Controller extends BaseController
 {
+
+/**
+ * @OA\OpenApi(
+ *  @OA\Info(
+ *      title="Returns Services API",
+ *      version="1.0.0",
+ *      description="API documentation for Returns Service App",
+ *      @OA\Contact(
+ *          email="sushil@stepfront.com"
+ *      )
+ *  ),
+ *  @OA\Server(
+ *      description="Returns App API",
+ *      url="https://localhost/api/"
+ *  ),
+ *  @OA\PathItem(
+ *      path="/"
+ *  )
+ * )
+ */
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Integration Swagger in Laravel with Passport Auth Documentation",
+     *      description="Implementation of Swagger with in Laravel",
+     *      @OA\Contact(
+     *          email="admin@admin.com"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Demo API Server"
+     * )
+
+     *
+     *
+     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
